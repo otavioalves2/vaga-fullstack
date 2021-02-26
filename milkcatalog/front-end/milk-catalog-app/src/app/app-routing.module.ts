7dd1266fded480c3948cbe84c26a11a574344a97
Otávio Alves <otavioalves124@gmail.com>
2021-02-26 20:57:38 -0300

    [feat] added routing

--- milkcatalog/front-end/milk-catalog-app/src/app/app-routing.module.ts
+++ milkcatalog/front-end/milk-catalog-app/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductComponent } from './product/product.component';
+import { SearchComponent } from './search/search.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+{ path: '', redirectTo: '/search', pathMatch: 'full' },
+{ path: 'search', component: SearchComponent },
+{ path: 'product', component: ProductComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
